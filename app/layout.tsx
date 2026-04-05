31439336d7c3470d4e9373a3334a8d298e786cf9
Thomas Nis Westergaard <69501259+ThomasNisWestergaard@users.noreply.github.com>
2026-04-05 09:44:17 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Planning Poker",
+  title: "FaciliAid: Planning Poker",
   description: "Planning poker starter for Supabase + Vercel",
 };
 
